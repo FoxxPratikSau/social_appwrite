33a5af605e8650d2c61556a80ab57e7d70b4cd4f
FoxxPratikSau <pratik.sau09@gmail.com>
2023-05-31 00:10:06 +0530

    home screen ui

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,6 +4,7 @@ import 'package:microblogging_appwrite/theme/pallete.dart';
 class AppTheme {
   static ThemeData theme = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: Pallete.backgroundColor,
+    useMaterial3: false,
     appBarTheme: const AppBarTheme(
       backgroundColor: Pallete.backgroundColor,
       elevation: 0,
